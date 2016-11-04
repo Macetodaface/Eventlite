a56401256d88c0e86e27aed5ceece34950e1debf
Chris Grabowski <cgrabows@andrew.cmu.edu>
2016-11-04 16:57:18 -0400

    More template changes.

--- src/webapps/EventLite/models.py
+++ src/webapps/EventLite/models.py
@@ -2,42 +2,49 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class UserDetail(models.Model):
-     user = models.OneToOneField(User, on_delete = models.CASCADE)
-     seller = models.OneToOneField("Seller", blank=True)
-     buyer = models.OneToOneField("Buyer", blank=True)
-     icon = models.ImageField(upload_to = 'icons', blank = True)
-     bio = models.CharField(max_length = 420, default = '', blank = True)
-     email = models.EmailField()
-     social_login = models.BooleanField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    seller = models.OneToOneField("Seller", blank=True)
+    buyer = models.OneToOneField("Buyer", blank=True)
+    icon = models.ImageField(upload_to='icons', blank=True)
+    bio = models.CharField(max_length=420, default='', blank=True)
+    email = models.EmailField()
+    social_login = models.BooleanField()
+
 
 class Seller(models.Model):
-    eventsHosting = models.ForeignKey("Event")
+    eventsHosting = models.ForeignKey("Event", blank=True)
+
 
 class Buyer(models.Model):
-    eventsInterested = models.ForeignKey("Event")
-    ticketsPurchased = models.ForeignKey("Ticket")
-    points = models.IntegerField(default = 0)
+    eventsInterested = models.ForeignKey("Event", blank=True)
+    ticketsPurchased = models.ForeignKey("Ticket", blank=True)
+    points = models.IntegerField(default=0)
+
 
 class Event(models.Model):
-    numOfTickets = models.IntegerField(default = 0)
-    ticketsSold = models.IntegerField(default = 0)
-    description = models.CharField(max_length = 1000, default = '', blank = True)
-    location = models.CharField(max_length = 100, default = '', blank = True)
+    numOfTickets = models.IntegerField(default=0)
+    ticketsSold = models.IntegerField(default=0)
+    description = models.CharField(max_length=1000, default='', blank=True)
+    location = models.CharField(max_length=100, default='', blank=True)
     time = models.DateTimeField(default=timezone.now)
-    media = models.URLField(default = '', blank = True)
+    media = models.URLField(default='', blank=True)
     email = models.EmailField()
+
 
 class Ticket(models.Model):
     event = models.ForeignKey("Event")
     price = models.FloatField()
-    details = models.CharField(max_length = 1000)
+    details = models.CharField(max_length=1000)
+
 
 class Review(models.Model):
-    rating = models.IntegerField()
-    review = models.CharField(max_length = 420, default = '', blank = True)
-    event = models.ForeignKey("Event")
+    rating=models.IntegerField()
+    review=models.CharField(max_length=420, default='', blank=True)
+    event=models.ForeignKey("Event")
+
 
 class Image(models.Model):
-    event = models.ForeignKey("Event")
-    image = models.ImageField(upload_to = 'images', blank = True)
+    event=models.ForeignKey("Event")
+    image=models.ImageField(upload_to='images', blank=True)
--- src/webapps/EventLite/views.py
+++ src/webapps/EventLite/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from EventLite.models import *
+from django.contrib.auth.models import User
+from sys import stderr
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    b = Buyer(points=0)
+    user = User(username='', password='')
+    u = UserDetail(user = user, email='', social_login=False)
+    print("Hello", file=stderr)
+    return render(request, 'index.html', {'user': request.user})
 
 
 def base(request):
@@ -17,3 +22,6 @@ def post_event(request):
 
 def registration(request):
     return render(request, 'registration.html', {})
+
+def view_events(request):
+    return render(request, 'view-events.html', {})
