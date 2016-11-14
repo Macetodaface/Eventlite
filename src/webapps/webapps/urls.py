1f0168862b5433ad36d0bc9eef9e2e482466e762
Chris Grabowski <cgrabows@andrew.cmu.edu>
2016-11-14 16:27:49 -0500

    Refactored urls.py

--- src/webapps/webapps/urls.py
+++ src/webapps/webapps/urls.py
@@ -17,25 +17,28 @@ from django.conf.urls import include, url
 from django.contrib import admin
 import django.contrib.auth.views
 
-from EventLite import views
+import EventLite.views.eventViews as eventViews
+import EventLite.views.accountViews as accountViews
+import EventLite.views.views as views
+
 
 urlpatterns = [
 
-    url(r'^$', views.view_events,name='home'),
+    url(r'^$', eventViews.view_events,name='home'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^base', views.base, name='base'),
-    url(r'^post-event', views.post_event, name='post-event'),
-    url(r'^registration', views.registration, name='registration'),
+    url(r'^post-event', eventViews.post_event, name='post-event'),
+    url(r'^registration', accountViews.registration, name='registration'),
 
-    url(r'^view-events', views.view_events, name='view-events'),
-    url(r'^my-events', views.my_events, name='my-events'),
-    url(r'^event-info/(?P<id>.+)', views.event_info, name='event-info'),
+    url(r'^view-events', eventViews.view_events, name='view-events'),
+    url(r'^my-events', eventViews.my_events, name='my-events'),
+    url(r'^event-info/(?P<id>.+)', eventViews.event_info, name='event-info'),
 
-    url(r'^login/$', views.manual_login, name='login'),
-    url(r'^loggedin', views.social_login, name='loggedin'),
-    url(r'^logout', views.logoutUser, name='logout'),
-    url(r'^activate$', views.activate, name='activate'),
-    url(r'^recover-password', views.recover_password, name='recover-password'),
-    url(r'^new_password/(?P<key>.+)', views.new_password, name='new-password'),
-    url(r'^search-events/$', views.search_events, name='search-events'),
+    url(r'^login/$', accountViews.manual_login, name='login'),
+    url(r'^loggedin', accountViews.social_login, name='loggedin'),
+    url(r'^logout', accountViews.logoutUser, name='logout'),
+    url(r'^activate$', accountViews.activate, name='activate'),
+    url(r'^recover-password', accountViews.recover_password, name='recover-password'),
+    url(r'^new_password/(?P<key>.+)', accountViews.new_password, name='new-password'),
+    url(r'^search-events/$', eventViews.search_events, name='search-events'),
 ]
