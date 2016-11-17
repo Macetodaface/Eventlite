8a5e37b3eaa0c3acd27ac66ac200d55d97838827
Kamal <kamalanarayan91@gmail.com>
2016-11-16 20:27:43 -0500

    Add Google Maps integration to Post Event

--- src/webapps/EventLite/static/js/postEvent.js
+++ src/webapps/EventLite/static/js/postEvent.js
@@ -96,7 +96,43 @@ function setCsrf(){
     });
 }
 
+// Map Stuff
+
+window.initMap = function () {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat: -34.397, lng: 150.644}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('id_location').addEventListener('keypress', function(e)
+  {
+      if(e.keyCode == 13)
+      {
+          geocodeAddress(geocoder, map);
+      }
+});
+
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('id_location').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Enter a valid location! ');
+    }
+  });
+}
+
+
 $(document).ready(function () {
     setCsrf();
     addClickHandlers();
+
 });
